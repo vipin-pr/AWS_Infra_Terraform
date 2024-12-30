09af2a3609237173e9d7a9ab285c63defc289b4a
vipin-pr <vipinrajan77@gmail.com>
2024-12-30 23:13:19 +0530

    aws_lb and aws_lb_lister added

--- modules/aws_ec2_module/main.tf
+++ modules/aws_ec2_module/main.tf
@@ -36,8 +36,8 @@ resource "aws_security_group" "load-balancer-sg" {
 }
 
 
-resource "aws_lb_target_group" "aws-load-balancer" {
-  name     = "aws-load-balancer-${terraform.workspace}"
+resource "aws_lb_target_group" "aws-load-balancer-target-group" {
+  name     = "aws-load-balancer-target-group-${terraform.workspace}"
   port     = 80
   protocol = "HTTP"
   vpc_id = var.vpc_id 
@@ -68,4 +68,15 @@ resource "aws_lb" "load-balancer" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.load-balancer-sg.id]
   subnets            = var.public_subnet 
+}
+
+resource "aws_lb_listener" "terramino" {
+  load_balancer_arn = aws_lb.load-balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.aws-load-balancer-target-group.arn
+  }
 }
