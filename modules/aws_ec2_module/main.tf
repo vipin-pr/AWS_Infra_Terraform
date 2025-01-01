e1d14db152d8029c7fd8f82fd7042e8a54cdcf95
vipin-pr <vipinrajan77@gmail.com>
2025-01-01 15:53:44 +0530

    reference changed to module.vpc.vpc_id

--- modules/aws_ec2_module/main.tf
+++ modules/aws_ec2_module/main.tf
@@ -37,10 +37,10 @@ resource "aws_security_group" "load-balancer-sg" {
 
 
 resource "aws_lb_target_group" "aws-load-balancer-target-group" {
-  name     = "aws-load-balancer-target-group-${terraform.workspace}"
+  name     = "aws-load-balancer-tg-${terraform.workspace}"
   port     = 80
   protocol = "HTTP"
-  vpc_id = var.vpc_id 
+  vpc_id = module.vpc.vpc_id
 }
 
 resource "aws_launch_configuration" "launch-configuration" {
