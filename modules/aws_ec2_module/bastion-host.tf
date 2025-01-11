e8475c2c660e52a347f082b2b06e5ec43c41b5e5
vipin-pr <vipinrajan77@gmail.com>
2025-01-11 20:19:54 +0530

    protocol changed to -1 in bastion-host-sg

--- modules/aws_ec2_module/bastion-host.tf
+++ modules/aws_ec2_module/bastion-host.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "bastion-host-sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
